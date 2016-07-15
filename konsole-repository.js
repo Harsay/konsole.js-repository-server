0ba5dffe605692cc4622e6ae8dac2e63d15e151e
Michał Pietraszko <contact@harsay.com>
2016-07-16 00:01:55 +0100

    0.1.4 release

--- konsole-repository.js
+++ konsole-repository.js
@@ -16,9 +16,22 @@ function getCommand(name) {
   return commands.find(elem => elem.name === name);
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/get/:name', function (req, res) {
   let command = getCommand(req.params.name);
-  res.send(JSONfn.stringify(command));
+  if(typeof command === 'undefined')
+    res.status(404).send('Command not found');
+  else
+    res.send(JSONfn.stringify(command));
+});
+
+app.get('*', function (req, res) {
+    res.status(404).send('404 Not found');
 });
 
 app.listen(3000, function () {
